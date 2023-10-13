bbbcbe904d8b7142af5bbc2abe75d888ff3972ec
Felipe Freitag Vargas <ffvargas@gmail.com>
2023-10-13 11:14:16 -0300

    Add navlink

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
 import {
   Form,
-  Link,
+  NavLink,
   Links,
   LiveReload,
   Meta,
@@ -62,8 +62,16 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
-                      {contact.first || contact.last ? (
+                  <NavLink
+                  className={({ isActive, isPending }) =>
+                    isActive
+                      ? "active"
+                      : isPending
+                      ? "pending"
+                      : ""
+                  }
+                  to={`contacts/${contact.id}`}
+                >                      {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
                         </>
@@ -73,7 +81,7 @@ export default function App() {
                       {contact.favorite ? (
                         <span>★</span>
                       ) : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
